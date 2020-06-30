0774ca7c138184d1654163420346049fe100db99
AE <anya.a.ellis@gmail.com>
2020-06-30 10:15:16 -0400

    Intro testing. Did not run test so there could be code errors. Idea is understood though

---
+++ portfolio/src/main/java/com/google/sps/servlets/AccessServlet.java
@@ -0,0 +1,10 @@
+package com.google.sps.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/access")
+public class AccessServlet extends HttpServlet {
+}
